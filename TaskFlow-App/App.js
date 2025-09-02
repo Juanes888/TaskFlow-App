2428ee1794fbd406d144b23328d953635984a161
Juan Esteban Zuluaga <juan.zuluagaez@amigo.edu.co>
2025-09-01 21:59:48 -0500

    Nuevas Pantallas y funcionalidades
    Agregamos nuevas pantallas , junto a nuevas funcionalidades con estilos visuales

--- TaskFlow-App/App.js
+++ TaskFlow-App/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import SplashScreen from "./src/screens/SplashScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
+import UserScreen from "./src/screens/UserScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Inicio" component={HomeScreen} />
+      <Tab.Screen name="Usuario" component={UserScreen} />
+      <Tab.Screen name="Configuracion" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registro" component={RegisterScreen} />
+        <Stack.Screen name="Inicio" component={MainTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
